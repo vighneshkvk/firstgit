672b4c2d0ec1262c7c2665c6d9eaf2e05c4a1b43
vighneshkvk <vighneshkamath020>
2023-08-14 11:23:38 +0530

    getelementbyId

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 // console.dir(document);
 // console.log(document.title);
 
-var titleheader=document.getElementById('header-title');
+// var titleheader=document.getElementById('header-title');
 // titleheader.textContent='kvk';
 // console.log(titleheader);
 // titleheader.innerHTML='<h3>hello</h3>';
-titleheader.style.borderBottom='solid 2px black';
+// titleheader.style.borderBottom='solid 2px black';
+
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items.style.backgroundColor='yellow' it wont work
+
+// for(var i=0;i<items.length;i++)
+// {
+//     items[i].style.backgroundColor='yellow';
+// }
 
 var items=document.getElementsByClassName('list-group-item');
 console.log(items);
-// items.style.backgroundColor='yellow' it wont work
-
+items[2].style.backgroundColor='green'
 for(var i=0;i<items.length;i++)
 {
-    items[i].style.backgroundColor='yellow';
+    items[i].style.fontWeight='bold';
+
 }
