e6d488d8246f1d3462de0e4832e27fc0985fe600
vighneshkvk <vighneshkamath020>
2023-08-14 11:11:56 +0530

    latest code

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('I love love git');
+// console.dir(document);
+// console.log(document.title);
+
+var titleheader=document.getElementById('header-title');
+// titleheader.textContent='kvk';
+// console.log(titleheader);
+// titleheader.innerHTML='<h3>hello</h3>';
+titleheader.style.borderBottom='solid 2px black';
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+// items.style.backgroundColor='yellow' it wont work
+
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor='yellow';
+}
