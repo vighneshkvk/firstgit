42dcb60f48e55fecd0d636adf861ae8ce5571ba2
vighneshkvk <vighneshkamath020>
2023-08-14 14:49:43 +0530

    queryselectorall

--- app.js
+++ app.js
@@ -35,8 +35,18 @@
 // }
 
 // QuerySelectors
-var  secondItem= document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor='green';
+// var  secondItem= document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='green';
 
-var  thirdItem= document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display='none';
+// var  thirdItem= document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display='none';
+
+// queryselectorall
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='green';
+}
+
+var secondli = document.querySelectorAll('.list-group-item');
+secondli[1].style.color='green';
