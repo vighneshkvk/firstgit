0691d4f46469393c00be9c9819af7d4e7cf41aaf
vighneshkvk <vighneshkamath020>
2023-08-16 16:26:21 +0530

    localstorage object

--- bookingapp.js
+++ bookingapp.js
@@ -1,11 +1,15 @@
 function test(event){
     event.preventDefault();
-    let uid=document.getElementById("username").value;
-    let pw=document.getElementById("password").value;
+    let name=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
     let email=document.getElementById("email").value;
     
-    let user=localStorage.setItem('uid',uid);
-    let pass=localStorage.setItem('pw',pw);
-    let em=localStorage.setItem('email',email);
+    let userDetails={
+        name,
+        password,
+        email
+    }
+
+    localStorage.setItem('userDetails', JSON.stringify(userDetails));
 
 }
