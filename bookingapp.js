0ca2453a4986601a13e8114825f758c08983e80a
vighneshkvk <vighneshkamath020>
2023-08-16 20:03:45 +0530

    delete user

--- bookingapp.js
+++ bookingapp.js
@@ -1,6 +1,8 @@
 var form=document.getElementById('addform');
 var itemList=document.getElementById('items');
 
+itemList.addEventListener('click',removeItem);
+
 function test(event){
     event.preventDefault();
     let name=document.getElementById("username").value;
@@ -18,6 +20,14 @@ function test(event){
     li.appendChild(document.createTextNode(password));
     li.appendChild(document.createTextNode(email));
 
+    // creating delete btn
+    var deletebtn=document.createElement('button');
+    deletebtn.className='btn btn-danger btn-sm float-right delete';
+    deletebtn.appendChild(document.createTextNode('delete'));
+    li.appendChild(deletebtn);
+   
+    
+
     itemList.appendChild(li);
     
     let userDetails={
@@ -27,7 +37,28 @@ function test(event){
     }
 
     localStorage.setItem(userDetails.email, JSON.stringify(userDetails));
+   
+    
 
+}
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        var li=e.target.parentElement;
+        itemList.removeChild(li);
+    }
+
+    let name=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+    let email=document.getElementById("email").value;
+
+    let userDetails={
+        name,
+        password,
+        email
+    }
+
+    localStorage.removeItem(userDetails.email, JSON.stringify(userDetails));
 }
 
 
