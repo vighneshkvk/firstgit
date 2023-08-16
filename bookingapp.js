928c4614f8e03e8bd746afc9f94a700421b7a1b8
vighneshkvk <vighneshkamath020>
2023-08-16 17:35:20 +0530

    scale to more user

--- bookingapp.js
+++ bookingapp.js
@@ -1,8 +1,24 @@
+var form=document.getElementById('addform');
+var itemList=document.getElementById('items');
+
 function test(event){
     event.preventDefault();
     let name=document.getElementById("username").value;
     let password=document.getElementById("password").value;
     let email=document.getElementById("email").value;
+
+    // create li
+    var li=document.createElement('li');
+
+    // give class name to li
+    li.className='list-group-item';
+
+    // add text node
+    li.appendChild(document.createTextNode(name));
+    li.appendChild(document.createTextNode(password));
+    li.appendChild(document.createTextNode(email));
+
+    itemList.appendChild(li);
     
     let userDetails={
         name,
@@ -10,6 +26,11 @@ function test(event){
         email
     }
 
-    localStorage.setItem('userDetails', JSON.stringify(userDetails));
+    localStorage.setItem(userDetails.email, JSON.stringify(userDetails));
 
 }
+
+
+
+
+
